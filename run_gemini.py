e6175607d4fa25f9b65cc04e8e811bedd61be554
agis09 <agis09m@gmail.com>
2024-05-30 17:41:53 +0900

    fix:

--- run_gemini.py
+++ run_gemini.py
@@ -1,5 +1,6 @@
 import google.generativeai as genai
 import os
+import subprocess
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
@@ -10,125 +11,23 @@ model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 def start_chat(content):
     chat = model.start_chat()
-    response = chat.send_message(
-        content=content
-    )
+    response = chat.send_message(content=content)
     return response
 
 
-content="""
+command = "git diff main..dev"
+git_diff = subprocess.run(command, shell=True, stdout=subprocess.PIPE, check=True)
+
+content = f"""
 You are a great software engineer. Please review my code based on the following `git diff main..HEAD` results and changed source code. The review should state what needs to be corrected and why for the areas that need to be corrected. Then, please output the review results in the following json format
 ```json
-{“body”: “review comments”, “path”: “target filename”, “line”: “target line number”, “side”: “whether comments are for deletions or additions (LEFT for comments on deleted sources, RIGHT for comments on added sources)”}
+{{“body”: “review comments”, “path”: “target filename”, “line”: “target line number”, “side”: “whether comments are for deletions or additions (LEFT for comments on deleted sources, RIGHT for comments on added sources)”}}
 ```
 Results of git diff:
 ```
-diff --git a/chat.py b/chat.py
-index 9797aaa..1291909 100644
---- a/chat.py
-+++ b/chat.py
-@@ -75,19 +75,4 @@ def download_image(url, timeout=10):: return
-     return response.content
- 
- 
--# determine the filename of the image
--def make_filename(base_dir, number, url):
-- ext = os.path.splitext(url)[1] # get extension
-- filename = number + ext # make filename by number with extension
--filename = fullpath = os.path.splitext(url)[1
-- fullpath = os.path.join(base_dir, filename)
-- return fullpath
-- fullpath = os.path.join(base_dir, filename)
--# save image
--# Save an image
--def save_image(filename, image):.
-- with open(filename, “wb”) as fout:
-- fout.write(image)
--# save_image(filename, image)
--def save_image(filename, image)
- client.run(os.environ[“DISCORD_API_TOKEN”])
-```
-```chat.py
-import google.generativeai as genai
-import os
-import discord
-import requests
-from PIL import Image
-from io import BytesIO
-from dotenv import load_dotenv
-
-load_dotenv(override=True)
-
-intents = discord.Intents.all()
-client = discord.Client(intents=intents)
-
-genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-model = genai.GenerativeModel("gemini-1.0-pro-vision-latest")
-
-chat = None
-
-
-def init_chat():
-    global chat
-    chat = model.start_chat()
-    response = chat.send_message(
-        content="You are a excellent chat bot. Please chat me in Japanese!"
-    )
-
-
-@client.event
-async def on_ready():
-    # init_chat()
-    print(f"{client.user.name} is online!")
-
-
-@client.event
-async def on_message(message):
-    print(f"Message received from {message.author}")
-    if message.author == client.user or not (str(client.user.id) in message.content):
-        return
-
-    content = message.content.strip()
-    content = content.replace(f"<@{str(client.user.id)}> ", "")
-    print(f"content: {content}")
-    attachments = message.attachments
-
-    if content == "reflesh":
-        init_chat()
-        await message.reply("refleshed chat !!!!")
-
-    elif attachments:
-        print(attachments)
-        img_url = attachments[0].url
-        res = requests.get(img_url)
-        img = Image.open(BytesIO(res.content))
-        response = model.generate_content([img, content])
-
-        await message.reply(response.text)
-
-    else:
-        # response = chat.send_message(content=[img, content])
-        print(response.text)
-        await message.reply(response.text)
-
-
-def download_image(url, timeout=10):
-    response = requests.get(url, allow_redirects=False, timeout=timeout)
-    if response.status_code != 200:
-        e = Exception("HTTP status: " + response.status_code)
-        raise e
-
-    content_type = response.headers["content-type"]
-    if "image" not in content_type:
-        e = Exception("Content-Type: " + content_type)
-        raise e
-
-    return response.content
-
-
-client.run(os.environ["DISCORD_API_TOKEN"])
+{git_diff.stdout}
 ```
 No additional text is required. Only output the above json format.
 """
-res=start_chat(content)
+res = start_chat(content)
 print(res.text)
